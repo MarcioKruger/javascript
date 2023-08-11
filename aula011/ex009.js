a195ae88bf165165dd51559f53aa654a38a43049
MarcioKruger <ms.kruger@hotmail.com>
2023-08-11 11:37:34 -0300

    exercicos

---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'japaõ'
+console.log(`Vivnedo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log(`você é brasileiro`)
+}else{
+    console.log(`Vovê é estrangeiro`)
+}
---
+++ aula011/index.js
@@ -0,0 +1,7 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel}km/h por hora.`)
+if (vel > 60){
+    console.log(`você ultrapassou a velocidade. MULTADO`)
+}else{
+    console.log(`diriga sempre usando cinto de segurança`)
+}
